ef9c48ddb9a34ecc83d1ad4f7c57fff4f93c25b2
Drendal911 <76110710+Drendal911@users.noreply.github.com>
2021-12-27 15:16:09 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import {img_1, img_2, img_3, img_4, img_5} from "./images";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <body>
+            <div className="container">
+                <div id="content-slider">
+                    <div id="slider">
+                        <ul>
+                            <li id="first" className="firstanimation">
+                                <img src={img_1} alt="Girls Volleyball Preparation"/>
+                            </li>
+
+                            <li id="second" className="secondanimation">
+                                <img src={img_2} alt="Girls Volleyball Celebration"/>
+                            </li>
+
+                            <li id="third" className="thirdanimation">
+                                <img src={img_3} alt="Boys Basketball Shooting"/>
+                            </li>
+
+                            <li id="fourth" className="fourthanimation">
+                                <img src={img_4} alt="Boys Basketball Free Throw"/>
+                            </li>
+
+                            <li id="fifth" className="fifthanimation">
+                                <img src={img_5} alt="Basketball Referee"/>
+                            </li>
+                        </ul>
+                        <div className="progress-bar"/>
+                    </div>
+                    {/*End Slider Container*/}
+                </div>
+                {/*End Slider-Content*/}
+                <div className="test">
+                    HELLO WORLD!
+                </div>
+            </div>
+            </body>
+        </div>
+    );
 }
 
 export default App;
