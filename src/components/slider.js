23f47fd22f3cf3ada59b973d7fbbb5a3d708d1da
Drendal911 <76110710+Drendal911@users.noreply.github.com>
2021-12-27 16:32:59 -0600

    converted to scss

--- src/components/slider.js
+++ src/components/slider.js
@@ -8,23 +8,23 @@ export default function Slider() {
         <div className={slider.contentSlider}>
             <div className="slider">
                 <ul>
-                    <li id="first" className="firstanimation">
+                    <li className="firstanimation">
                         <img src={img_1} alt="Girls Volleyball Preparation"/>
                     </li>
 
-                    <li id="second" className="secondanimation">
+                    <li className="secondanimation">
                         <img src={img_2} alt="Girls Volleyball Celebration"/>
                     </li>
 
-                    <li id="third" className="thirdanimation">
+                    <li className="thirdanimation">
                         <img src={img_3} alt="Boys Basketball Shooting"/>
                     </li>
 
-                    <li id="fourth" className="fourthanimation">
+                    <li className="fourthanimation">
                         <img src={img_4} alt="Boys Basketball Free Throw"/>
                     </li>
 
-                    <li id="fifth" className="fifthanimation">
+                    <li className="fifthanimation">
                         <img src={img_5} alt="Basketball Referee"/>
                     </li>
                 </ul>
