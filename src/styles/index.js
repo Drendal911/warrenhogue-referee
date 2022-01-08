cabee98d163801fed09aa9531d537262c06fcbc9
Drendal911 <76110710+Drendal911@users.noreply.github.com>
2022-01-08 05:50:18 -0600

    Delete index.js

--- src/styles/index.js
+++
@@ -1,9 +0,0 @@
-import slider from "./components/slider.scss";
-import card from "./components/card.scss";
-import footer from "./components/footer.scss"
-
-export {
-    slider,
-    card,
-    footer
-}
